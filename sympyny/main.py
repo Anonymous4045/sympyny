39776db36ba2b61fcc8cbdd62bad8aedba17b7c0
Anonymous <Unanimous4045@proton.me>
2023-04-05 22:27:06 -0500

    Change main.py to new objective
    This commit changes the main file to reflect my new vision for this project. I'm essentially tossing out everything before this in favor of this new approach, where the song is generated first and then played. This allows for a much more consistent song and allows me to do other things with the music.

--- sympyny/main.py
+++ sympyny/main.py
@@ -1,7 +1,37 @@
-from pathlib import Path
+"""What the end result should be able to do (subject to change)."""
 
-PATHS = {
-    "root": Path(__file__).parent.parent,
-    "sounds": Path(__file__).parent.parent / "sounds",
-    "brass": Path(__file__).parent.parent / "sounds" / "brass",
-}
+from sympyny import Song
+from sympyny.instruments import trumpet
+
+
+# Song definition
+
+# Define song with name and other necessary stuff
+ode_to_joy = Song("Ode to Joy", bpm=120, octive=5, composer="Anonymous4045")
+
+# Add a musical layer comprised of one instrument. Can be as many layers as needed.
+ode_to_joy.add_layer("Melody", trumpet, (("C5", 1 / 2), ("F5", 1 / 4), ("Rest", 1)))
+
+
+# Processing
+
+# Runs through all the processing to add the sounds defined in the layers above into the given format
+ode_to_joy.save("ode_to_joy", "wav")
+
+
+# Usage
+
+# Plays the sound file saved above with the given parameters
+ode_to_joy.play(volume=0.5, speed=1)
+
+
+# Fun stuff I might add
+
+# Create an image of the sheet music for each layer of the song
+ode_to_joy.generate_sheet_music("Ode to Joy Sheet Music", "pdf")
+
+# Turn the melody of the song into a piano tiles game
+ode_to_joy.play_piano_tiles()
+
+# Play the song with animations of various things, like graphs of the frequency or pitch
+ode_to_joy.visualize()
